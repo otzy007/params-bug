09f3399cf55e32ceba1a84c31c454a5628bf9ddb
Andrei Gliga <otzy_007@yahoo.com>
2013-09-04 22:27:13 +0300

    neither params.key? work

--- params-bug/app/controllers/shout_controller.rb
+++ params-bug/app/controllers/shout_controller.rb
@@ -1,14 +1,15 @@
 class ShoutController < ApplicationController
    def create
       p 'Parametri'
+  
       p params[:shout]
-      shout = params[:shout]
-   
-      if shout.nil? || shout == {}
+      
+      unless params.key? :shout
 	 render :json => { :errors => ['No shout message'] }, :status => :bad_request
 	 return
       end
-      
+      shout = params[:shout]
+   
       render :json => { :errors => ['Done'] }, :status => :ok
    end
 end
